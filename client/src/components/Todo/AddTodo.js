15e9157b83821852cb3b617bbaf1c688e23c8d98
Seulg2027 <seulgi2027@dgu.ac.kr>
2022-05-28 16:16:19 +0900

    🎨[Design] 데시보드 UI 변경, Router를 component루 변경

---
+++ client/src/components/Todo/AddTodo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// UI components //
+import AddIcon from '@mui/icons-material/Add';
+
+function AddTodo() {
+  return (
+    <>
+      <AddIcon className="text-2xl ml-4 mb-1"></AddIcon>
+      <br />
+    </>
+  );
+}
+
+export default AddTodo;
