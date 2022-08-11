ffa07bdd38a952ec7bb3644490d7e7fddd0f1413
Seulg2027 <seulgi2027@dgu.ac.kr>
2022-08-11 21:50:00 +0900

    🚧[Refactor] 로그인 Theme 리팩토링

--- client/src/layouts/signin/SignIn.js
+++ client/src/layouts/signin/SignIn.js
@@ -6,8 +6,6 @@ import { LOGIN_REQUEST } from 'redux/types.js';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import LockIcon from '@mui/icons-material/Lock';
 import Button from '@mui/material/Button';
-import { pointColor } from 'styles/color';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 // Google Login //
 import { GoogleLogin } from 'react-google-login';
@@ -15,14 +13,6 @@ import GoogleIcon from '@mui/icons-material/Google';
 
 // img //
 import iconImage from 'assets/img/ms-icon-70x70.png';
-
-const theme = createTheme({
-  palette: {
-    point: {
-      main: pointColor,
-    },
-  },
-});
 
 function SignIn(req) {
   const { isAuthenticated } = useSelector((state) => state.auth);
@@ -58,100 +48,98 @@ function SignIn(req) {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <div className="container">
-        <section className="bg-gray-200 h-screen w-screen flex justify-center items-center">
-          <div className="shadow-lg w-96 rounded-lg h-3/5 bg-white">
-            <div className="text-center">
-              <div className="flex flex-col justify-center">
-                <img
-                  className="text-center w-12"
-                  src={iconImage}
-                  style={{ margin: '25px auto' }}
-                />
-                <h2 className="text-3xl font-medium">로그인</h2>
-                <form method="POST" id="login-form">
-                  <div className="form-group mt-5">
-                    <label for="user_id">
-                      <AccountBoxIcon className="text-xl"></AccountBoxIcon>
-                    </label>
-                    <input
-                      type="text"
-                      className="input-sm"
-                      name="user_id"
-                      id="user_id"
-                      placeholder="ID"
-                      onChange={onChangeValue}
+    <div className="container">
+      <section className="bg-gray-200 h-screen w-screen flex justify-center items-center">
+        <div className="shadow-lg w-96 rounded-lg h-3/5 bg-white">
+          <div className="text-center">
+            <div className="flex flex-col justify-center">
+              <img
+                className="text-center w-12"
+                src={iconImage}
+                style={{ margin: '25px auto' }}
+              />
+              <h2 className="text-3xl font-medium">로그인</h2>
+              <form method="POST" id="login-form">
+                <div className="form-group mt-5">
+                  <label for="user_id">
+                    <AccountBoxIcon className="text-xl"></AccountBoxIcon>
+                  </label>
+                  <input
+                    type="text"
+                    className="input-sm"
+                    name="user_id"
+                    id="user_id"
+                    placeholder="ID"
+                    onChange={onChangeValue}
+                  />
+                </div>
+                <div className="form-group">
+                  <label for="user_password">
+                    <LockIcon></LockIcon>
+                  </label>
+                  <input
+                    type="password"
+                    className="input-sm"
+                    name="user_password"
+                    id="user_password"
+                    placeholder="Password"
+                    onChange={onChangeValue}
+                  />
+                </div>
+                <div className="form-group form-button mt-5">
+                  <Button
+                    type="submit"
+                    className="w-3/5"
+                    name="signin"
+                    id="signin"
+                    variant="outlined"
+                    onClick={onSubmit}
+                    color="point"
+                  >
+                    Log in
+                  </Button>
+                </div>
+              </form>
+              <div className="mt-3">
+                <ul className="socials">
+                  <li>
+                    <GoogleLogin
+                      clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                      buttonText="구글로 계속하기"
+                      onSuccess={responseGoogle}
+                      onFailure={responseGoogle}
+                      isSignedIn={true}
+                      render={(renderProps) => (
+                        <Button
+                          type="submit"
+                          className="w-3/5"
+                          variant="outlined"
+                          color="point"
+                          startIcon={<GoogleIcon />}
+                          onClick={renderProps.onClick}
+                        >
+                          구글로 로그인하기
+                        </Button>
+                      )}
                     />
-                  </div>
-                  <div className="form-group">
-                    <label for="user_password">
-                      <LockIcon></LockIcon>
-                    </label>
-                    <input
-                      type="password"
-                      className="input-sm"
-                      name="user_password"
-                      id="user_password"
-                      placeholder="Password"
-                      onChange={onChangeValue}
-                    />
-                  </div>
-                  <div className="form-group form-button mt-5">
+                  </li>
+                  <li className="signin-image mt-3">
                     <Button
                       type="submit"
                       className="w-3/5"
-                      name="signin"
-                      id="signin"
-                      variant="outlined"
-                      onClick={onSubmit}
-                      color="point"
+                      variant="contained"
+                      href="/signup"
                     >
-                      Log in
+                      회원가입
                     </Button>
-                  </div>
-                </form>
-                <div className="mt-3">
-                  <ul className="socials">
-                    <li>
-                      <GoogleLogin
-                        clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-                        buttonText="구글로 계속하기"
-                        onSuccess={responseGoogle}
-                        onFailure={responseGoogle}
-                        isSignedIn={true}
-                        render={(renderProps) => (
-                          <Button
-                            type="submit"
-                            className="w-3/5"
-                            variant="outlined"
-                            color="point"
-                            startIcon={<GoogleIcon />}
-                            onClick={renderProps.onClick}
-                          >
-                            구글로 로그인하기
-                          </Button>
-                        )}
-                      />
-                    </li>
-                    <li className="signin-image mt-3">
-                      <Button
-                        type="submit"
-                        className="w-3/5"
-                        variant="contained"
-                        href="/signup"
-                      >
-                        회원가입
-                      </Button>
-                    </li>
-                  </ul>
-                </div>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
-        </section>
-      </div>
-    </ThemeProvider>
+        </div>
+      </section>
+    </div>
   );
 }
 
