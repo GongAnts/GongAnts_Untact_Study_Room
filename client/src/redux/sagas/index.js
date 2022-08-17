f1394a73c59e7501b7d7baa7de9721f4d66ce72b
Seulg2027 <seulgi2027@dgu.ac.kr>
2022-08-17 23:53:11 +0900

    ❌[Remove] 메모 기능 삭제

--- client/src/redux/sagas/index.js
+++ client/src/redux/sagas/index.js
@@ -3,15 +3,9 @@ import axios from 'axios';
 
 import dotenv from 'dotenv';
 import authsaga from './authsaga';
-import memosaga from './memosaga';
 import schedulesaga from './schedulesaga';
 import todosaga from './todosaga';
 
 export default function* rootSaga() {
-  yield all([
-    fork(authsaga),
-    fork(memosaga),
-    fork(schedulesaga),
-    fork(todosaga),
-  ]);
+  yield all([fork(authsaga), fork(schedulesaga), fork(todosaga)]);
 }
