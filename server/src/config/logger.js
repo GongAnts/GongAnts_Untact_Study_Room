e4d1db5fc5771970221e2bffcaf9aca6e17c879b
olive-su <1466su@naver.com>
2022-09-30 15:15:16 +0900

    ➕[Feat] logger exceptionHandler, stream 추가

--- server/src/config/logger.js
+++ server/src/config/logger.js
@@ -1,12 +1,17 @@
+require('dotenv').config();
 const winston = require('winston');
 const winstonDaily = require('winston-daily-rotate-file');
-const { combine, timestamp, printf, colorize } = winston.format;
+const appRoot = require('app-root-path');
+const process = require('process');
 
-const logDir = 'logs'; // logs 디렉토리 하위에 로그 파일 저장
+const logDir = `${appRoot}/logs`;
+
+const { combine, timestamp, printf, colorize, simple } = winston.format;
 
 const logFormat = printf((info) => {
   return `${info.timestamp} ${info.level}: ${info.message}`;
 });
+
 /*
  * Log Level
  * error: 0, warn: 1, info: 2, http: 3, verbose: 4, debug: 5, silly: 6
@@ -19,25 +24,15 @@ const logger = winston.createLogger({
     logFormat,
   ),
   transports: [
-    // info 레벨 로그를 저장할 파일 설정
     new winstonDaily({
       level: 'info',
       datePattern: 'YYYY-MM-DD',
       dirname: logDir,
       filename: `%DATE%.log`,
-      maxFiles: 30,
+      maxFiles: 30, // 30일 단위
       zippedArchive: true,
     }),
-    // warn 레벨 로그를 저장할 파일 설정
-    new winstonDaily({
-      level: 'warn',
-      datePattern: 'YYYY-MM-DD',
-      dirname: logDir + '/warn',
-      filename: `%DATE%.warn.log`,
-      maxFiles: 30,
-      zippedArchive: true,
-    }),
-    // error 레벨 로그를 저장할 파일 설정
+
     new winstonDaily({
       level: 'error',
       datePattern: 'YYYY-MM-DD',
@@ -47,10 +42,20 @@ const logger = winston.createLogger({
       zippedArchive: true,
     }),
   ],
+
+  exceptionHandlers: [
+    new winstonDaily({
+      level: 'error',
+      datePattern: 'YYYY-MM-DD',
+      dirname: logDir + '/error',
+      filename: `%DATE%.exception.log`,
+      maxFiles: 30,
+      zippedArchive: true,
+    }),
+  ],
 });
 
 logger.stream = {
-  // morgan wiston 설정
   write: (message) => {
     logger.info(message);
   },
@@ -59,12 +64,9 @@ logger.stream = {
 if (process.env.NODE_ENV !== 'production') {
   logger.add(
     new winston.transports.Console({
-      format: combine(
-        colorize({ all: true }), // console 에 출력할 로그 컬러 설정 적용함
-        logFormat, // log format 적용
-      ),
+      format: combine(colorize(), simple()),
     }),
   );
 }
 
-export { logger };
+module.exports = logger;
