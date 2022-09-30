bae364516410f3378f9b00c4e1b46fa8f8b344c2
olive-su <1466su@naver.com>
2022-09-30 19:03:58 +0900

    ➕[Feat] passport logger 추가

--- server/src/config/passport/localLogin.js
+++ server/src/config/passport/localLogin.js
@@ -1,6 +1,7 @@
 const LocalStrategy = require('passport-local').Strategy;
 const dao = require('../../dao/auth');
 const bcrypt = require('bcrypt');
+const logger = require('../logger');
 
 // Local Passport 정의
 module.exports = new LocalStrategy(
@@ -14,11 +15,12 @@ module.exports = new LocalStrategy(
       { id: 'u' + userid, password: password },
       function (err, data) {
         if (err) {
+          logger.error(`[localLogin] ${err}`);
           return done(err);
         } else {
           if (data === undefined) {
-            console.log('계정이 존재하지 않습니다.');
-            return done(null, false, { error: '계정이 존재하지 않습니다.' });
+            logger.error('[localLogin] 계정이 존재하지 않습니다.');
+            return done(null, false);
           } else {
             // user_password 일치 확인
             const json = JSON.stringify(data);
@@ -28,10 +30,8 @@ module.exports = new LocalStrategy(
               if (result) {
                 return done(null, data);
               } else {
-                console.log('비밀번호가 일치하지 않습니다.');
-                return done(null, false, {
-                  error: '비밀번호가 일치하지 않습니다.',
-                });
+                logger.error('[localLogin] 비밀번호가 일치하지 않습니다.');
+                return done(null, false);
               }
             });
           }
